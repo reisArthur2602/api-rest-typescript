a883051d824678238c1dbac80cf4b9441ab04a6a
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-01 14:02:44 -0300

    feat: add createRoom e videoRepository as rotas

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { roomRepository } from '../repositories/RoomRepository';
+import { videoRepository } from '../repositories/VideoRepository';
 
 export class RoomController {
   async create(req: Request, res: Response) {
@@ -8,6 +9,22 @@ export class RoomController {
       const room = roomRepository.create({ name, description });
       await roomRepository.save(room);
       return res.status(201).json(room);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+
+  async createRoom(req: Request, res: Response) {
+    const { title, url } = req.body;
+    const { room_id } = req.params;
+    try {
+      const room = await roomRepository.findOneBy({ id: parseInt(room_id) });
+      if (!room) return res.status(404).json({ messsage: 'Aula não existe' });
+
+      const video = videoRepository.create({ title, url, room });
+      await videoRepository.save(video);
+      return res.status(201).json(video);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: 'Internal Server Error' });
--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import { SubjectController } from './controllers/SubjectController';
 import { RoomController } from './controllers/RoomController';
 
+
 const routes = Router();
 routes.post('/subject', new SubjectController().create);
 routes.post('/room', new RoomController().create);
+routes.post('/room/:room_id/create', new RoomController().createRoom);
 
 export default routes;
