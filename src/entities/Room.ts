9ca3be261d394d9adce67886a440e8890c575872
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-01 13:17:04 -0300

    feat: add entities

---
+++ src/entities/Room.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Video } from './Video';
+import { Subject } from './Subject';
+
+@Entity('rooms')
+export class Room {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @OneToMany(() => Video, (video) => video.room)
+  videos: Video[];
+
+  @ManyToMany(() => Subject, (subject) => subject.rooms)
+  subjects: Subject[];
+}
