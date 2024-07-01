065235809761f9fedc0cf5e575b01f6a3d1e1224
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-01 16:36:17 -0300

    feat: remover try catch

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -2,74 +2,61 @@ import { Request, Response } from 'express';
 import { roomRepository } from '../repositories/RoomRepository';
 import { videoRepository } from '../repositories/VideoRepository';
 import { subjectRepository } from '../repositories/SubjectRepository';
+import { BadRequestError, NotFoundError } from '../helpers/error';
 
 export class RoomController {
   async create(req: Request, res: Response) {
     const { name, description } = req.body;
-    try {
-      const room = roomRepository.create({ name, description });
-      await roomRepository.save(room);
-      return res.status(201).json(room);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    if (!name || !description)
+      throw new BadRequestError('Preencha os dados corretamente');
+    const room = roomRepository.create({ name, description });
+    await roomRepository.save(room);
+    return res.status(201).json(room);
   }
 
   async createRoom(req: Request, res: Response) {
     const { title, url } = req.body;
     const { room_id } = req.params;
-    try {
-      const room = await roomRepository.findOneBy({ id: parseInt(room_id) });
-      if (!room) return res.status(404).json({ messsage: 'Aula não existe' });
 
-      const video = videoRepository.create({ title, url, room });
-      await videoRepository.save(video);
-      return res.status(201).json(video);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    if (!title || !url || !room_id)
+      throw new BadRequestError('Preencha os dados corretamente');
+
+    const room = await roomRepository.findOneBy({ id: parseInt(room_id) });
+    if (!room) throw new NotFoundError('Aula não existe');
+
+    const video = videoRepository.create({ title, url, room });
+    await videoRepository.save(video);
+    return res.status(201).json(video);
   }
 
   async roomSubject(req: Request, res: Response) {
     const { subject_id } = req.body;
     const { room_id } = req.params;
 
-    try {
-      const room = await roomRepository.findOneBy({ id: parseInt(room_id) });
-      if (!room) return res.status(404).json({ messsage: 'Aula não existe' });
+    const room = await roomRepository.findOneBy({ id: parseInt(room_id) });
+    if (!room) throw new NotFoundError('Aula não existe');
 
-      const subject = await subjectRepository.findOneBy({ id: subject_id });
-      if (!subject)
-        return res.status(404).json({ messsage: 'Disciplina não existe' });
+    const subject = await subjectRepository.findOneBy({ id: subject_id });
+    if (!subject) throw new NotFoundError('Disciplina não existe');
 
-      const roomUpdate = {
-        ...room,
-        subjects: [subject],
-      };
-      await roomRepository.save(roomUpdate);
+    const roomUpdate = {
+      ...room,
+      subjects: [subject],
+    };
+    await roomRepository.save(roomUpdate);
 
-      return res.status(204).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    return res.status(204).send();
   }
 
   async list(req: Request, res: Response) {
-    try {
-      const rooms = await roomRepository.find({
-        relations: {
-          subjects: true,
-          videos: true,
-        },
-      });
+    const rooms = await roomRepository.find({
+      relations: {
+        subjects: true,
+        videos: true,
+      },
+    });
+    if (!rooms) throw new NotFoundError('Nenhuma aula foi encontrada');
 
-      return res.status(200).json(rooms);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    return res.status(200).json(rooms);
   }
 }
--- src/controllers/SubjectController.ts
+++ src/controllers/SubjectController.ts
@@ -1,21 +1,15 @@
 import { Request, Response } from 'express';
 import { subjectRepository } from '../repositories/SubjectRepository';
+import { BadRequestError } from '../helpers/error';
 
 export class SubjectController {
   async create(req: Request, res: Response) {
     const { name } = req.body;
+    
+    if (!name) throw new BadRequestError('O nome é obrigatório');
 
-    if (!name) return res.status(400).json({ message: 'O nome é obrigatório' });
-
-    try {
-      const subject = subjectRepository.create({ name });
-      await subjectRepository.save(subject);
-
-      return res.status(201).json(subject);
-      
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    const subject = subjectRepository.create({ name });
+    await subjectRepository.save(subject);
+    return res.status(201).json(subject);
   }
 }
